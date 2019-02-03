045425af0530170ca3b1ab8911ab82ca39e781a9
MrHyd3V1L <47284440+mrhydevil@users.noreply.github.com>
2019-02-03 14:21:30 -0300

    Update ssh-connec-client.py

--- chapter2/ssh-connec-client.py
+++ chapter2/ssh-connec-client.py
@@ -6,7 +6,7 @@ import subprocess
 
 def ssh_command(ip,user,passwd,command):
     client = paramiko.SSHClient()
-    client.load_host_keys('/home/ghostnil/.ssh/known_hosts')
+    #client.load_host_keys('/home/$USER/.ssh/known_hosts')
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip,username=user,password=passwd)
     ssh_session = client.get_transport().open_session()
